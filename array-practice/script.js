62316e67b5a0066d373b1233ab83cf6302e9ac48
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-04-29 19:09:56 +0000

    committed

---
+++ array-practice/script.js
@@ -0,0 +1,20 @@
+
+
+let arr = [1,2,3,4,5];
+
+console.log(arr);
+console.log(arr.length);
+arr[0] = 11;
+
+arr[1] = 22;
+
+arr[2] = 33;
+
+arr[3] = 44;
+
+arr[4] = 55;
+
+arr[5] = 66;
+
+console.log(arr);
+console.log(arr.length);
