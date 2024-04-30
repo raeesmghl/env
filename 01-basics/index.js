6a6a42c2ce156f86cc0c59e06ad0b8142c484b6c
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-04-30 01:22:21 +0000

    committed

---
+++ 01-basics/index.js
@@ -0,0 +1,20 @@
+
+// javascript github setup
+// variables
+// operators
+// conditionals
+// loops
+// strings
+// arrays
+// functions
+// dom
+// asynchronous programming
+// callbacks
+// promises
+// async/await
+// 
+
+
+
+///////////////////////////////
+
