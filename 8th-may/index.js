0248ce6786a729c4a2dccf093448e75e0b393999
Raees Mughal <raees.mughal.official@gmail.com>
2024-09-22 14:21:02 +0000

    new function added

--- 8th-may/index.js
+++ 8th-may/index.js
@@ -28,4 +28,33 @@ function func(str) {
 }
 
 let a = func(myStr);
-console.log(a);
+// console.log(a);
+
+
+
+function isPrime(n){
+    if(n<2){
+        return false;
+    }
+
+    for(let i = 2 ;i<n;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(1))
+console.log(isPrime(2))
+console.log(isPrime(3))
+console.log(isPrime(4))
+console.log(isPrime(5))
+console.log(isPrime(6))
+console.log(isPrime(7))
+console.log(isPrime(8))
+console.log(isPrime(9))
+console.log(isPrime(10))
+console.log(isPrime(11))
+
